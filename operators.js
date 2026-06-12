e9894a32b3aecd0974301d9fd1b5943e3aa044ed
Dharika04 <dharikap25csa@vetias.ac.in>
2026-06-12 14:01:30 +0530

    operators

--- operators.js
+++ operators.js
@@ -1,4 +1,4 @@
-// arithmetic operators
+// \Arithmetic operators
 var a=10;
 var b=40;
 console.log("Arithmetic operator");
@@ -6,12 +6,12 @@ console.log(a+b); //addition
 console.log(a-b); //subtraction
 console.log(a*b); //multiplication
 console.log(a/b); //division
-console.log(a%b); //modulo
+console.log(a%b); //modulos
 console.log(a**b); //exponent 
 
 //Assignment operator
 var a=50;
-console.log("assignment operator");
+console.log("Assignment operator");
 console.log(a+=5); //addition assignment 
 console.log(a-=5); //subtraction assignment 
 console.log(a*=5); // multiplication assignment
@@ -19,8 +19,8 @@ console.log(a/=5); // division assignment
 console.log(a%=5); // mmodulo assignment 
 console.log(a**=5); // exponent assignment 
 
-// comparison operator 
-console.log("comparison operator");
+// Comparison operator 
+console.log("Comparison operator");
 console.log(a==b); //equal to
 console.log(a!=b); //not equal to
 console.log(a<=b);  // less than or equal to 
@@ -34,3 +34,16 @@ console.log(a>10 && b>30); //logical And
 console.log(a>10 || b>30); //logical or
 console.log(!a>10); //logical not 
 
+
+// Unary operator
+console.log("unary operator");
+console.log(a++);
+console.log(a--);
+console.log(--a);
+console.log(++a);
+
+
+// Conditional/Ternary operator
+console.log("Conditional operator");
+var result=(a>b)? "a is greater" : "b is greater";
+console.log(result);
