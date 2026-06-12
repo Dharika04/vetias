b67d7f50fddd919676ae16eb5de7041383e947c9
Dharika04 <dharikap25csa@vetias.ac.in>
2026-06-12 11:23:40 +0530

    operators

--- operators.js
+++ operators.js
@@ -6,4 +6,13 @@ console.log(a-b); //subtraction
 console.log(a*b); //multiplication
 console.log(a/b); //division
 console.log(a%b); //modulo
-console.log(a**b); //exponentcls
+console.log(a**b); //exponent 
+
+//Assignment operator
+var a=50;
+console.log(a+=5); //addition assignment 
+console.log(a-=5); //subtraction assignment 
+console.log(a*=5); // multiplication assignment
+console.log(a/=5); // division assignment 
+console.log(a%=5); // mmodulo assignment 
+console.log(a**=5); // exponent assignment 
