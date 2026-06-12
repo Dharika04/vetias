c6d628f8a89c2926a7cf98de89144fffa5d83dec
Dharika04 <dharikap25csa@vetias.ac.in>
2026-06-12 11:39:14 +0530

    operators.js

--- operators.js
+++ operators.js
@@ -10,9 +10,17 @@ console.log(a**b); //exponent
 
 //Assignment operator
 var a=50;
+console.log("assignment operator");
 console.log(a+=5); //addition assignment 
 console.log(a-=5); //subtraction assignment 
 console.log(a*=5); // multiplication assignment
 console.log(a/=5); // division assignment 
 console.log(a%=5); // mmodulo assignment 
 console.log(a**=5); // exponent assignment 
+
+// comparison operator 
+console.log("comparison operator");
+console.log(a==b); //equal to
+console.log(a!=b); //not equal to
+console.log(a<=b);  // less than or equal to 
+console.log(a>=b);  // greater than or equal to
