c70cc1cd8e76d24f5faaf18dbc39f57a704e3de9
Dharika04 <dharikap25csa@vetias.ac.in>
2026-06-12 10:56:33 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,9 @@
+// arithmetic operators
+var a=10;
+var b=40;
+console.log(a+b); //addition
+console.log(a-b); //subtraction
+console.log(a*b); //multiplication
+console.log(a/b); //division
+console.log(a%b); //modulo
+console.log(a**b); //exponentcls
