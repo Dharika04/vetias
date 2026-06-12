6da976aaa0b3a84a3039e4eac1a8582afc96d513
Dharika04 <dharikap25csa@vetias.ac.in>
2026-06-12 15:03:22 +0530

    datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,56 @@
+//DATATYPES
+console.log("Primitive Datatypes")
+
+//Numbers
+console.log(" DT Numbers")
+var age=30
+console.log(age)
+
+//Strings
+console.log(" DT Strings")
+var name="hello"
+console.log(name)
+
+//Boolean
+console.log("DT Boolean")
+var student =true
+console.log(student)
+
+//BigInt
+console.log("DT BigInt")
+var Numbers=123456767888545443
+console.log(Numbers)
+
+//Null
+console.log("DT Null")
+var Null=null
+console.log(null)
+
+//Symbol
+console.log("DT Symbol")
+var symbol="#"
+console.log(symbol)
+
+//Undefined
+console.log("DT Undefined")
+var Undefined="undefined"
+console.log(undefined)
+
+
+//Non Primitive type
+console.log("Non primitive data type")
+
+//Array
+console.log("DT Array")
+var array =[1,2,3,4,5]
+console.log(array)
+
+//Objects
+console.log("DT Objects")
+var person = {
+    name:"dha",
+    age: 20,
+    
+}
+console.log(person.age);
+
