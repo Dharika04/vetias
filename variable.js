66158889bc9a34448576041de8c12e82c4ae5383
Dharika04 <dharikap25csa@vetias.ac.in>
2026-06-12 10:21:23 +0530

    variable

---
+++ variable.js
@@ -0,0 +1,13 @@
+// variable declaration
+var name ="vetias"
+console.log(name)
+
+// variable declaration using lrt
+{
+    let age="30"
+    console.log(age)
+}
+
+//variable declaration using const 
+const days="monday"
+console.log(days)
